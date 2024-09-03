9bb85bc5643d2c26a53538ab6c57b2c986af4223
thortek <ta.anderson@gmail.com>
2024-09-03 17:13:10 -0600

    Renamed the repo to 3380-sveltekit-app and added some basic types examples

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,55 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
+  // var name = 'world';
+  let foo: any = 'foo'
+  const bar = ['baz', 'qux']
+
+  foo = {} // you can reassign foo because it's type is 'any'
+  bar.push('quux') // you can modify the array, just not reassign it
+
+  const dataTypeComment = `TypeScript adds several data types on top of JavaScript's data types:
+  - boolean
+        - number
+        - string
+        - array
+        - tuple
+        - enum
+        - any
+        - void
+        - null and undefined
+        - never
+        - object `
+
+let isDone: boolean = false; // Assignment operator is '='.  Read it as "is assigned the value of"
+let num1 = 3
+let num2 = 3
+let num3 = num1 + num2
+
+let myArray: number[] = [1, 2, 3, 4, 5]; // Array of numbers
+let myArray2: Array<number> = [1, 2, 3, 4, 5]; // Array of numbers
+
+let myTuple: [string, number, string] = ['hello', 10, 'world']; // Tuple
+
+enum Color {
+  Red,
+  Bahama_Green,
+  Island_Teal,
+  Quartz
+}
+
+const sportsCar = {
+	make: 'Porsche',
+  model: '911',
+  year: 2020,
+  color: Color.Quartz
+}
+
+//JSON.stringify(sportsCar)
+
+</script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-5">
-		<h1 class="text-4xl text-slate-400">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
+		<h1 class="text-4xl text-slate-400">Welcome to my JavaScript Site!</h1>
+		<p class="text-lg text-slate-400">Result is {Color[2]}!</p>
 	</div>
 </div>
