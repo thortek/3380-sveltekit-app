499db880608281c770a0fc41d4df839ddf3d70ac
thortek <ta.anderson@gmail.com>
2024-09-19 16:59:17 -0600

    Got some reactivitity issues with bind:value going on

--- src/routes/loops/+page.svelte
+++ src/routes/loops/+page.svelte
@@ -1,29 +1,25 @@
 <script lang="ts">
 	import { CodeBlock } from '@skeletonlabs/skeleton';
 
-	function filteredArray(arr: any, elem: any) {
-		let newArr: any = [];
+  let newArr: string
+  let inputArray = JSON.stringify([[10, 8, 3],[14, 6, 23],[3, 18, 6]])
+  let element = 18
+
+  $: console.log(inputArray, element)
+
+	function filterTheArray() {
+		let userArray = JSON.parse(inputArray)
+    let tempArr = []
 		// Removes any nested array that contains the element
-		for (let i = 0; i < arr.length; i++) {
-			if (!arr[i].includes(elem)) {
-				newArr.push(arr[i]);
+		for (let i = 0; i < userArray.length; i++) {
+			if (!userArray[i].includes(element)) {
+				tempArr.push(userArray[i])
 			}
 		}
-
-		return newArr;
+    newArr = JSON.stringify(tempArr)
+    console.log(newArr)
 	}
 
-    function filterTheArray() {
-        const result = filteredArray(
-            [
-                [10, 8, 3],
-                [14, 6, 23],
-                [3, 18, 6],
-            ],
-            14
-        );
-        return result
-    }
 </script>
 
 <h1>Loops</h1>
@@ -46,6 +42,8 @@
 ></CodeBlock>
 
 <div class="flex m-4">
-    <input class="input w-32" type="text" id="input" />
+    <input bind:value={inputArray} class="input w-96 mr-4" type="text" id="input" placeholder="[[10, 8, 3],[14, 6, 23],[3, 18, 6]]" />
+    <input bind:value={element} class="input w-32 mr-4" type="text" id="input" placeholder="23" />
     <button class="btn variant-filled-primary" id="submit" on:click={filterTheArray}>Change the Element</button>
+    <input bind:value={newArr} class="input w-96 ml-4" type="text"/>
 </div>
