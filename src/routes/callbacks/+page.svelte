e4516134b56f4c7ae9c8c6d878fd93a28068ab68
thortek <ta.anderson@gmail.com>
2024-09-26 17:14:08 -0600

    Can write a single pokemon to disk.  new callbacks route added.

---
+++ src/routes/callbacks/+page.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+    import { CodeBlock } from '@skeletonlabs/skeleton';
+
+function fetchData(callback: any) {
+    // Simulate an asynchronous API call or request using setTimeout
+    setTimeout(() => {
+        // Call the callback function and pass the data
+        callback('bar', 'Data from the API');
+    }, 2000)
+}
+
+// Now let's call the fetchData function and pass a callback function
+fetchData((error: any, data: any) => {
+    if (error) {
+        console.error(error)
+    } else {
+        console.log(data);
+    }
+});
+
+// Fetch data from the pokeUrl using Promises
+function fetchPokeDataByPromise() {
+    fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
+
+</script>
+
+<h1>Callback Examples</h1>
+
+<!-- Show the code examples using the CodeBlock component -->
+
+<CodeBlock
+	class="m-4"
+	language="ts"
+	code={`function fetchData(callback: any) {
+    // Simulate an asynchronous API call or request using setTimeout
+    setTimeout(() => {
+        // Call the callback function and pass the data
+        callback('bar', 'Data from the API');
+    }, 2000)
+}`}
+></CodeBlock>
--- src/scripts/fetchPokeData.ts
+++ src/scripts/fetchPokeData.ts
@@ -1,27 +1,28 @@
 const pokeUrl = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151';
+import { promises as fsPromises } from 'fs';
 
-// Fetch data from the pokeUrl using Promises
-function fetchPokeDataByPromise() {
-    fetch(pokeUrl)
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
-}
 
 // Fetch data from the pokeUrl using async/await
 async function fetchPokeData() {
 	try {
-		const response = await fetch(pokeUrl);
+		const response = await fetch(`https://pokeapi./api/v2/pokemon/1`);
 		const data = await response.json();
-		console.log(data);
+		fsPromises.writeFile('data/pokemon.json', JSON.stringify(data, null, 2));
 	} catch (error) {
 		console.error('Error:', error);
 	}
 }
+
+async function fetchAllPokemonData(pokemonArray: { name: string, url: string }[]) {
+    const pokemonData = await Promise.all(
+        pokemonArray.map(async (pokemon) => {
+            const response = await fetch(pokemon.url);
+            return response.json();
+        })
+    );
+    //console.log(pokemonData);
+}
+
 
 async function main() {
     const startTime = new Date()
@@ -37,5 +38,6 @@ async function main() {
 }
 
 await main();
+
 export { };
 
