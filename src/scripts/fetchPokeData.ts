71e0e526af86775c6852163f24731430ffae952e
thortek <ta.anderson@gmail.com>
2024-10-01 17:14:45 -0600

    Got started on pokemon reduction. Used an .env file

--- src/scripts/fetchPokeData.ts
+++ src/scripts/fetchPokeData.ts
@@ -1,13 +1,22 @@
-const pokeUrl = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151';
+
 import { promises as fsPromises } from 'fs';
+import 'dotenv/config'
+
+const pokeUrl = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151';
+
+//console.log('Hello from fetchPokeData.ts', process.env);
+
+let initialPokeArray = [];
 
 
 // Fetch data from the pokeUrl using async/await
 async function fetchPokeData() {
 	try {
-		const response = await fetch(`https://pokeapi./api/v2/pokemon/1`);
+		const response = await fetch(`https://pokeapi.co/api/v2/pokemon/?offset=725&limit=25`);
 		const data = await response.json();
-		fsPromises.writeFile('data/pokemon.json', JSON.stringify(data, null, 2));
+        initialPokeArray = data.results;
+        fetchAllPokemonData(initialPokeArray);
+		//fsPromises.writeFile('data/pokemon.json', JSON.stringify(data, null, 2));
 	} catch (error) {
 		console.error('Error:', error);
 	}
@@ -17,10 +26,17 @@ async function fetchAllPokemonData(pokemonArray: { name: string, url: string }[]
     const pokemonData = await Promise.all(
         pokemonArray.map(async (pokemon) => {
             const response = await fetch(pokemon.url);
-            return response.json();
+            const data = await response.json();
+            const reducedPokemon = {
+                name: pokemon.name,
+                id: data.id,
+                height: data.height,
+                weight: data.weight,
+            }
+            return reducedPokemon;
         })
     );
-    //console.log(pokemonData);
+    console.log(pokemonData);
 }
 
 
@@ -30,6 +46,7 @@ async function main() {
     // Fetch data using Promises
     //fetchPokeDataByPromise();
     await fetchPokeData();
+    console.log(`initialPokeArray length is: ${initialPokeArray.length}`);
     const endTime = new Date()
     const elapsedTime = endTime.getTime() - startTime.getTime()
     const elapsedTimeInSeconds = elapsedTime / 1000
