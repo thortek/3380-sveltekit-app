acbf434f71ee21e21b8ac13beb5a7228c7178020
thortek <ta.anderson@gmail.com>
2024-11-05 17:10:15 -0700

    Got basic chat with Ollama working... like a Boss!

---
+++ src/routes/api/chat/+server.ts
@@ -0,0 +1,27 @@
+import OpenAI from 'openai'
+
+const openai = new OpenAI({
+    baseURL: 'http://localhost:11434/v1',
+    apiKey: 'ollama', // required but unused
+})
+
+export const POST = async ({ request }) => {
+    const body = await request.json()
+    console.log(body)
+
+    const completion = await openai.chat.completions.create({
+        model: 'llama3.2',
+        messages: [
+            { role: 'user', content: body.message}
+        ]
+    })
+
+    console.log(completion.choices[0].message.content)
+
+    return new Response(JSON.stringify({ message: completion.choices[0].message.content }), {
+        status: 200,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+}
---
+++ src/routes/chat/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+
+let inputChat = ''
+let answerText = ''
+
+async function handleSubmit(this: HTMLFormElement, event: Event) {
+       // event.preventDefault();
+		const response = await fetch('/api/chat', {
+            method: 'POST',
+            body: JSON.stringify({
+                message: inputChat,
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        console.log(response)
+        const data = await response.json()
+        answerText = data.message
+	};
+
+</script>
+
+<h1>Chat happens here</h1>
+
+<form on:submit={handleSubmit}>
+    <input class="input m-2" type="text" placeholder="Type a message" bind:value={inputChat} />
+    <p>{answerText}</p>
+    <button class="btn variant-filled-primary m-2" type="submit">Send Chat</button>
+</form>
