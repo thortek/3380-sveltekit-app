187a2765d68435d4493ad36c8b48cd08a7256b8d
thortek <ta.anderson@gmail.com>
2024-11-12 17:11:25 -0700

    Getting the UI to work with the streamed answer data - part 1

--- src/routes/chat/+page.svelte
+++ src/routes/chat/+page.svelte
@@ -2,12 +2,18 @@
 	import { marked } from 'marked';
 	import { chatHistoryStore } from '$lib/stores/chatHistoryStore';
 	import { readableStreamStore } from '$lib/stores/readableStreamStore';
+	import { Avatar } from '@skeletonlabs/skeleton';
+	import { fly } from 'svelte/transition'
 
 	let inputChat = '';
 	let answerText = '';
 
+	const response = readableStreamStore();
+
 	async function handleSubmit(this: HTMLFormElement, event: Event) {
 		event.preventDefault();
+		if ($response.loading) return;
+
 		const formData: FormData = new FormData(this);
 		const message = formData.get('message') as string;
 
@@ -16,33 +22,73 @@
 
 		$chatHistoryStore = [...$chatHistoryStore, { role: 'user', content: message }];
 
-		const response = await fetch('/api/chat', {
-			method: 'POST',
-			body: JSON.stringify({
-				chats: $chatHistoryStore
-			}),
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		});
-		console.log(response);
-		const data = await response.json();
-		answerText = await marked.parse(data.message);
+		try {
+			const answer = response.request(
+				new Request('/api/chat', {
+					method: 'POST',
+					body: JSON.stringify({
+						chats: $chatHistoryStore
+					}),
+					headers: {
+						'Content-Type': 'application/json'
+					}
+				})
+			);
+
+			this.reset();
+
+			const answerText = (await answer) as string;
+
+			$chatHistoryStore = [...$chatHistoryStore, { role: 'assistant', content: answerText }];
+		} catch (error) {
+			console.error(error);
+			$chatHistoryStore = [
+				...$chatHistoryStore,
+				{ role: 'assistant', content: `Sorry, I am not able to answer that. ${error}` }
+			];
+		}
 	}
 </script>
 
-<h1>Chat happens here</h1>
-
-<form on:submit={handleSubmit}>
-	<input
-		class="input m-2"
-		type="text"
-		name="message"
-		placeholder="Type a message"
-		bind:value={inputChat}
-	/>
-	<button class="btn variant-filled-primary m-2" type="submit">Send Chat</button>
+<main class="w-screen h-full flex flex-col items-center">
+<form class="flex flex-col space-y-4 w-full max-w-7xl p-1" on:submit={handleSubmit}>
+	<div>
+		<!-- 		<input
+			class="input m-2"
+			type="text"
+			name="message"
+			placeholder="Type a message"
+			bind:value={inputChat}
+		/>
+		<button class="btn variant-filled-primary m-2" type="submit">Send Chat</button> -->
+		{#await new Promise((res) => setTimeout(res, 1000)) then _}
+			<div class="flex space-x-2">
+				<Avatar class="" src={'/img-tutor-girl.png'} rounded="rounded-full" width="w-12" />
+				<div in:fly={{ y: 50, duration: 1000}} class="assistant-chat">
+					Hello! How can I help you?
+				</div>
+			</div>
+		{/await}
+		<div class="space-y-4">
+			<hr />
+			<div class="flex space-x-4">
+				<textarea class="textarea" placeholder="Type your message..." name="message" rows="3" />
+				<div class="flex flex-col justify-between">
+					<button class="btn variant-filled-primary" type="submit">Send</button>
+					<button class="btn variant-outline-primary" type="reset">Clear Chats</button>
+				</div>
+			</div>
+		</div>
+	</div>
 </form>
-<div>
-	{@html answerText}
-</div>
+</main>
+
+<style lang="postcss">
+	.assistant-chat {
+		@apply bg-gray-200 text-gray-800 rounded-lg p-2
+	}
+
+	.user-chat {
+		@apply bg-slate-800 text-white
+	}
+</style>
