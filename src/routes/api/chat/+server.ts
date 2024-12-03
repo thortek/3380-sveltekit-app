cc3a128074c8821e475e222856b088659d63ca94
thortek <ta.anderson@gmail.com>
2024-12-03 16:30:45 -0700

    Changed models

--- src/routes/api/chat/+server.ts
+++ src/routes/api/chat/+server.ts
@@ -1,13 +1,13 @@
 import OpenAI from 'openai';
 
 // Create a new OpenAI instance to connect with your OpenAI API key
-// const openai = new OpenAI({apiKey: process.env.OPENAI_API_KEY})
+const openai = new OpenAI({apiKey: process.env.OPENAI_API_KEY})
 
 // Create a new OpenAI instance with this config for Ollama
-const openai = new OpenAI({
+/* const openai = new OpenAI({
 	baseURL: 'http://localhost:11434/v1',
 	apiKey: 'ollama' // required but unused
-});
+}); */
 
 export type MessageBody = { chats: { role: 'user' | 'assistant'; content: string }[] };
 
@@ -43,9 +43,9 @@ export const POST = async ({ request }) => {
 	console.log(body.chats);
 
 	const stream = await openai.chat.completions.create({
-		model: 'llama3.2',
+		model: 'gpt-4o',
 		messages: [
-			{ role: 'system', content: 'You are a helpful pirate who loves to use emojis in their response.'}, //rubberDuckPrompt },
+			{ role: 'system', content: 'You are a helpful web development expert.'}, //rubberDuckPrompt },
 			{ role: 'assistant', content: 'Hello! How can I help you today?' },
 			...body.chats
 		],
